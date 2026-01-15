30d8f48c555a2a8d5e25213c3280007102f3f67b
Julia Skip <skipjulija01@gmail.com>
2026-01-15 16:29:10 +0200

    Initial Commit

---
+++ AIRecipeFinder/AIRecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIRecipeFinderApp.swift
+//  AIRecipeFinder
+//
+//  Created by Скіп Юлія Ярославівна on 15.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct AIRecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
